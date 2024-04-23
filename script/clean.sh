cfede30292d241bc601fa081679531d5d4d36400
FourbeFlo <floriane.goyus@gmail.com>
2024-04-23 09:15:24 +0200

    mv script in the new rep.

---
+++ script/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+rm -rf train_data
+rm -rf Lambertus
---
+++ script/deploy-krak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+module load GCCcore/11.2.0 Python/3.9.6
+
+python3 -m venv kraken-env
+source ~/kraken-env/bin/activate
+
+pip install --upgrade pip
+pip install kraken albumentations
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+deactivate
