6148668577e4ef640ce142fa0de619d9987aa7b3
FourbeFlo <floriane.goyus@gmail.com>
2024-04-28 20:38:41 +0200

    change module load

--- script/sub_script_debug.sh
+++ script/sub_script_debug.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=8
 #SBATCH --gres=gpu:2,VramPerGpu:24GB
 
-module load fosscuda/2020b GCCcore/11.2.0 GCCcore/11.3.0 Python/3.9.6
+module load CUDA/11.8.0 GCCcore/11.2.0 GCCcore/11.3.0 Python/3.9.6
 source ~/kraken-env/bin/activate
 wget https://github.com/Gallicorpora/Segmentation-and-HTR-Models/releases/download/1.0.0/Gallicorpora+_best.mlmodel
 
