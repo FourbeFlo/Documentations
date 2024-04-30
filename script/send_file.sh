5cbf4c2291a3b160bd9c866f5f3ea202c05cd951
FourbeFlo <floriane.goyus@gmail.com>
2024-04-30 10:46:56 +0200

    try

--- script/send_file.sh
+++ script/send_file.sh
@@ -4,7 +4,7 @@
 remote_user="goyf"  # Remote server username
 remote_host="login2.baobab"  # Remote server IP or hostname
 remote_file="/home/users/g/goyf/best_model.mlmodel"  # Path to the file on the remote server
-local_dir="/home/floriane/Documents/Lambert_Daneau"  # Local directory where you want to save the file
+local_dir="/home/floriane/Documents"  # Local directory where you want to save the file
 
 # Download the file using scp
 scp "$remote_user"@"$remote_host":"$remote_file" "$local_dir/"
