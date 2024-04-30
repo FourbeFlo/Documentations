d1c74eb3dd511878e7942906f79d5aa43c78bede
FourbeFlo <floriane.goyus@gmail.com>
2024-04-30 11:08:28 +0200

    update

--- script/deploy-krak.sh
+++ script/deploy-krak.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-module load CUDA/11.8.0 GCCcore/11.2.0 Python/3.9.6
+#deployment of the virtual environement
 
+#choose the module 
+#for now we are on Python 3.9.6 but it could change and you have to find the right modules
+module load CUDA/11.8.0 GCCcore/11.3.0 Python/3.9.6
+
+#create the env
 python3 -m venv kraken-env
 source ~/kraken-env/bin/activate
 
+# install the right packages
 pip install --upgrade pip
 pip install kraken albumentations
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/env bash
+# create the train data if FONDUE-LA-PRINT-16 and Lambertus contain different data
+
+#downlaod the repository
+git clone https://github.com/FourbeFlo/Lambertus
 git clone https://github.com/FoNDUE-HTR/FONDUE-LA-PRINT-16
 git clone https://github.com/FoNDUE-HTR/FONDUE-FR-PRINT-16
+
+# create the train directory
 mkdir train_data
 cd train_data
 mkdir fondue
@@ -9,5 +15,7 @@ cd ..
 mv FONDUE-LA-PRINT-16/data/* train_data/fondue
 mv FONDUE-FR-PRINT-16/data/* train_data/fondue
 mv Lambertus/data/* train_data/new
+
+# clean 
 rm -rf FONDUE-LA-PRINT-16
 rm -rf FONDUE-FR-PRINT-16
