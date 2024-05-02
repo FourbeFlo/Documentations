01a9bcee4ee54e2b1820e6546a6522d04c90d96d
FourbeFlo <floriane.goyus@gmail.com>
2024-05-02 17:21:38 +0200

    Update download-rn.erara.sh

--- Scripts_data/download-rn.erara.sh
+++ Scripts_data/download-rn.erara.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Define the base URL and range of URLs
+
 base_url="https://www.e-rara.ch/download/webcache/1000/"
-start_index=9556086
-end_index=9557819
+start_index=2012883
+end_index=2013443
 
 # Loop through the range of URLs and download each file
 for ((i=start_index; i<=end_index; i++)); do
@@ -19,3 +20,13 @@ for ((i=start_index; i<=end_index; i++)); do
 done
 
 echo "Download and renaming complete!"
+
+#using the script
+
+#in e-rara only the final part of the link is the file's identifiant
+#for section that you want to downlaod
+#take the identifiant of the first identifiant as start_index it looks like this "9557819"
+#take the indentifiant of the last indentifiant as end_index
+#the script downlaod alle the data inbetween 
+
+#more information on the generale Guideline "comming soon"
